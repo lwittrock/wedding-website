ca8f85b0cfc69d246c5edf75af4bdba9c6526cc7
Lars Wittrock <larwitt@gmx.de>
2025-12-03 17:42:07 +0100

    Updated google maps link and start for backend photo upload

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -20,7 +20,7 @@ export const CONFIG = {
   TRAVEL_LINKS: {
     VENUE: "https://www.google.com/maps/search/?api=1&query=Domaine+Des+Officiers+Vielsalm",
     TRAIN_STATION: "https://www.google.com/maps/search/?api=1&query=Gare+de+Vielsalm",
-    PARKING_MAIN: "https://www.google.com/maps/@50.2857449,5.9152632,153m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MTExNy4wIKXMDSoASAFQAw%3D%3D",
+    PARKING_MAIN: "https://www.google.com/maps/dir//50.2852819,5.916406/@50.2853432,5.9161639,32m/data=!3m1!1e3!4m2!4m1!3e2!5m1!1e1?entry=ttu&g_ep=EgoyMDI1MTEzMC4wIKXMDSoASAFQAw%3D%3D",
     PARKING_CHURCH: "https://www.google.com/maps/@50.288295,5.9146925,87m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI1MTExNy4wIKXMDSoASAFQAw%3D%3D",
   }
 };
